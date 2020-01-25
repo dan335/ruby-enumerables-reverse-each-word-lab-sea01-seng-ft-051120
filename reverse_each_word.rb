3fb570c4bc63b55848263db5189f128d8a5aa0f5
Daniel Phillips <danphi@gmail.com>
2020-01-25 20:00:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,19 @@
-def reverse_each_word(str)
-  new = ""
-  first = true
-  
-  str.split.each do |word|
-    if !first
-      new += " "
-    end
-    
-    new += word.reverse!
-    
-    first = false
-  end
-  
-  new
-end
+#def reverse_each_word(str)
+#  new = ""
+#  first = true
+#  
+#  str.split.each do |word|
+#    if !first
+#      new += " "
+#    end
+#    
+#    new += word.reverse!
+#    
+#    first = false
+#  end
+#  
+#  new
+#end
 
 
 def reverse_each_word(str)
